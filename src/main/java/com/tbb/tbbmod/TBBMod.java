011d9caa9184984225d513433ae7edaa0f5d5005
RealTheBeastBoss <jack.cumbers@outlook.com>
2022-01-30 23:34:35 +0000

    Added BeastBoss Buttons, Doors, Trapdoors and Pressure Plates Added them to Wood Changer Map (Doors won't work, unfortunately)

--- src/main/java/com/tbb/tbbmod/TBBMod.java
+++ src/main/java/com/tbb/tbbmod/TBBMod.java
@@ -2,23 +2,17 @@ package com.tbb.tbbmod;
 
 import com.tbb.tbbmod.block.ModBlocks;
 import com.tbb.tbbmod.item.ModItems;
-import net.minecraft.world.level.block.Block;
+import net.minecraft.client.renderer.ItemBlockRenderTypes;
+import net.minecraft.client.renderer.RenderType;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.InterModComms;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
-import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-
-import java.util.stream.Collectors;
 
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod(TBBMod.MOD_ID)
@@ -34,9 +28,15 @@ public class TBBMod
         ModItems.register(eventBus);
         ModBlocks.register(eventBus);
         eventBus.addListener(this::setup);
+        eventBus.addListener(this::setupClient);
 
         // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void setupClient(final FMLClientSetupEvent event) {
+        ItemBlockRenderTypes.setRenderLayer(ModBlocks.BEASTBOSS_DOOR.get(), RenderType.cutout());
+        ItemBlockRenderTypes.setRenderLayer(ModBlocks.BEASTBOSS_TRAPDOOR.get(), RenderType.cutout());
     }
 
     private void setup(final FMLCommonSetupEvent event)
